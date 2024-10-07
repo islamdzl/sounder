86a95d3aec58429a3773f82d7127694c8b9c95eb
IslamDZl <157886980+islamdzl@users.noreply.github.com>
2024-10-07 22:58:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ let size = 0
 var clients = {}
 
 app.use(express.static('./'));
-app.use(express.static('publ'));
+// app.use(express.static('publ'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '//publ//index.html')
 })
