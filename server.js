fec0e69ecc9ec4e2fe14dfa5d9215c2d215021b8
IslamDZl <157886980+islamdzl@users.noreply.github.com>
2024-10-07 23:08:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,9 @@ const map = new Map()
 let size = 0
 var clients = {}
 
-// app.use(express.static('./'));  tiktokmBDprEsiMnTLJwlldigjH32OdO8ql6QW.txt
-app.use(express.static('publ'));
-app.get('/tiktokmBDprEsiMnTLJwlldigjH32OdO8ql6QW.txt', (req, res) => {
-  res.sendFile(__dirname + '//publ//tiktokmBDprEsiMnTLJwlldigjH32OdO8ql6QW.txt')
-})
+app.use(express.static('./'));  // tiktokmBDprEsiMnTLJwlldigjH32OdO8ql6QW.txt
+// app.use(express.static('publ'));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '//publ//index.html')
 })
